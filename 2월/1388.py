dcc41c6d620cc250ec73de460b090ac6cf07a7da
gkdidms <gkdidms3320@gmail.com>
2022-02-21 23:30:11 +0900

    2월 21일

---
+++ 2월/1388.py
@@ -0,0 +1,26 @@
+
+n,m = map(int, input().split())
+board = []
+
+for _ in range(n):
+    board.append(list(input()))
+
+count = 0
+
+for i in range(n):
+    pre = "/"
+    for j in range(m):
+        if board[i][j] == '-':
+            if board[i][j] != pre:
+                count += 1
+        pre = board[i][j]
+
+for j in range(m):
+    pre = "/"
+    for i in range(n):
+        if board[i][j] == '|':
+            if board[i][j] != pre:
+                count += 1
+        pre = board[i][j]
+    
+print(count)
